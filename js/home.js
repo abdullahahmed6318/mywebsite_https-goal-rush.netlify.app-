1a3f62e07f07bc1f1519717cb1a67dab47c5e76c
abdalla ahmed <80337502+abdullahahmed6318@users.noreply.github.com>
2025-09-16 20:03:22 +0300

    Add files via upload

--- js/home.js
+++ js/home.js
@@ -51,7 +51,7 @@
                               <a class="url" style="${displayStyle}" data-url="${product.First_team}">
                               <div class="The_first__match">
                                 <img src="${product.First_img}" alt="">
-                              <p class="First_team"> ${product.name_team_First}</p>
+                              <p class="First_team">${product.name_team_First}</p>
                                 </div>
                                 <div class="The_Match_time">
                                     <h1>vs</h1>
@@ -251,7 +251,7 @@
                                     <h1>vs</h1>
                                   </div>
                                 <div class="The_eighth__match">
-                                  <img src="${product.Second_img}" alt="">
+                                  <img src="${product.eighth_img}" alt="">
                                 <p class="Second_team">${product.name_team_eighth}  </p>
                                   </div>
                                 
@@ -261,14 +261,14 @@
                       <span>
                               <a class="url" style="${displayStyle_Ninth}" data-url="${product.Ninth_team}">
                               <div class="The_Ninth__match">
-                                <img src="${product.First_img}" alt="">
+                                <img src="${product.Ninth_img}" alt="">
                               <p class="First_team"> ${product.name_team_Ninth}</p>
                                 </div>
                                 <div class="The_Match_time">
                                     <h1>vs</h1>
                                   </div>
                                 <div class="The_tenth__match">
-                                  <img src="${product.Second_img}" alt="">
+                                  <img src="${product.tenth_img}" alt="">
                                 <p class="Second_team">${product.name_team_tenth}  </p>
                                   </div>
                                 
