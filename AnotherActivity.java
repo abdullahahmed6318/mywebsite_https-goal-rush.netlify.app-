1e3be2f2c3747d628149c410c19414676bf54d58
abdalla ahmed <80337502+abdullahahmed6318@users.noreply.github.com>
2025-10-14 14:43:43 +0300

    Add files via upload

---
+++ AnotherActivity.java
@@ -0,0 +1,23 @@
+package com.example.app_goalrush;
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class AnotherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_another);
+
+
+    }
+}
